0abc5c85759394e4e5e6feb71a39e7a9a34b4dc5
Alex Perez <alex.perez@ufl.edu>
2022-09-19 08:47:22 -0400

    Added more debug info to txros

--- txros/src/txros/rosxmlrpc.py
+++ txros/src/txros/rosxmlrpc.py
@@ -189,7 +189,7 @@ class XMLRPCException(TxrosException):
 
         help_message = ""
         if exception.__class__.__name__ == "ClientConnectorError" and method_name == "requestTopic":
-            help_message = "It appears the node may not be able to find a node publishing the topic it is attempting to listen to. This likely means that the node publishing to the topic has died. Please check the publisher to the topic for issues and restart ROS."
+            help_message = f"It appears the node may not be able to find a node publishing the topic it is attempting to listen to. This likely means that the node publishing to the topic has died. Please check the publisher to the topic for issues and restart ROS.\n\nRequest body: {self.request_body}"
 
         super().__init__(f"An error occurred in the XMLRPC communication. The '{self.node_handle._name}' node attempted to call '{method_name}' resulting in an error. For help resolving this exception, please see the txros documentation." + (f"\n\n{help_message}" if help_message else ""), self.node_handle)
 
